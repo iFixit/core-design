fa2ef8f58676bb7bfbed530e6e2981790ee7ac74
Rich McCluskey <rich@dozuki.com>
2020-06-08 17:02:53 -0700

    Add React output target to stencil
    Whenever stencil builds it will automatically generate the React wrapper
    code for all the web components. This code is generated to a separate
    lerna package which can be published to NPM.

---
+++ packages/components-react/src/generated/components.ts
@@ -0,0 +1,26 @@
+/* eslint-disable */
+/* tslint:disable */
+/* auto-generated react proxies */
+import { createReactComponent } from './react-component-lib';
+
+import { JSX } from '@core-design/components';
+
+import { defineCustomElements, applyPolyfills } from '@core-design/components/loader';
+
+applyPolyfills().then(() => defineCustomElements());
+export const CoreAlert = /*@__PURE__*/createReactComponent<JSX.CoreAlert, HTMLCoreAlertElement>('core-alert');
+export const CoreBadge = /*@__PURE__*/createReactComponent<JSX.CoreBadge, HTMLCoreBadgeElement>('core-badge');
+export const CoreButton = /*@__PURE__*/createReactComponent<JSX.CoreButton, HTMLCoreButtonElement>('core-button');
+export const CoreCheckbox = /*@__PURE__*/createReactComponent<JSX.CoreCheckbox, HTMLCoreCheckboxElement>('core-checkbox');
+export const CoreDropdown = /*@__PURE__*/createReactComponent<JSX.CoreDropdown, HTMLCoreDropdownElement>('core-dropdown');
+export const CoreDropdownItem = /*@__PURE__*/createReactComponent<JSX.CoreDropdownItem, HTMLCoreDropdownItemElement>('core-dropdown-item');
+export const CoreIcon = /*@__PURE__*/createReactComponent<JSX.CoreIcon, HTMLCoreIconElement>('core-icon');
+export const CoreInput = /*@__PURE__*/createReactComponent<JSX.CoreInput, HTMLCoreInputElement>('core-input');
+export const CoreLabel = /*@__PURE__*/createReactComponent<JSX.CoreLabel, HTMLCoreLabelElement>('core-label');
+export const CoreProgress = /*@__PURE__*/createReactComponent<JSX.CoreProgress, HTMLCoreProgressElement>('core-progress');
+export const CoreRadio = /*@__PURE__*/createReactComponent<JSX.CoreRadio, HTMLCoreRadioElement>('core-radio');
+export const CoreSpinner = /*@__PURE__*/createReactComponent<JSX.CoreSpinner, HTMLCoreSpinnerElement>('core-spinner');
+export const CoreTag = /*@__PURE__*/createReactComponent<JSX.CoreTag, HTMLCoreTagElement>('core-tag');
+export const CoreTextarea = /*@__PURE__*/createReactComponent<JSX.CoreTextarea, HTMLCoreTextareaElement>('core-textarea');
+export const CoreToast = /*@__PURE__*/createReactComponent<JSX.CoreToast, HTMLCoreToastElement>('core-toast');
+export const CoreToggle = /*@__PURE__*/createReactComponent<JSX.CoreToggle, HTMLCoreToggleElement>('core-toggle');
--- packages/components/stencil.config.ts
+++ packages/components/stencil.config.ts
@@ -2,11 +2,16 @@ import { Config } from "@stencil/core";
 import { less } from "@stencil/less";
 import LessPluginAutoPrefix from "less-plugin-autoprefix";
 import { generateJsonDocs } from "./src/utils/customElementDocGenerator";
+import { reactOutputTarget } from '@stencil/react-output-target';
 
 export const config: Config = {
   namespace: "components",
   taskQueue: "async",
   outputTargets: [
+    reactOutputTarget({
+      componentCorePackage: '@core-design/components',
+      proxiesFile: '../components-react/src/generated/components.ts',
+    }),
     {
       type: "dist",
       esmLoaderPath: "../loader",
