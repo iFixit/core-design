25ad2f40ffa9aca0fdca89b9531cbfedcdccc4a1
Ian Rohde <ian.rohde@gmail.com>
2021-02-24 11:39:37 -0800

    v0.1.0

---
+++ packages/components-image/src/components/core-image/core-image.tsx
@@ -0,0 +1,52 @@
+import { Component, Prop, h } from '@stencil/core';
+
+@Component({
+  tag: 'core-image',
+  styleUrl: 'core-image.css',
+  shadow: true,
+})
+export class Image {
+  /**
+   * The image alt tag. [MDN](https://developer.mozilla.org/en-US/docs/Web/HTML/Element/img#attr-alt)
+   */
+  @Prop() alt: string;
+
+  /**
+   * The intrinsic height of the image, in pixels. Must be an integer without a unit. Overwritten with `height: auto` by default, but should be included.
+   */
+  @Prop() height: number;
+
+  /**
+   * The image source path without file extension. On browsers supporting `srcset`, `src` is treated as the fallback image with a 1x pixel density.
+   */
+  @Prop() src: string;
+
+  /**
+   * The intrinsic width of the image, in pixels. Must be an integer without a unit.
+   */
+  @Prop() width: number;
+
+  render() {
+    const { alt, src, height, width } = this;
+    const imageOptions = {
+      types: ['avif', 'webp', 'jpg'],
+    };
+
+    return (
+      <picture>
+        {imageOptions.types.map(type => (
+          <source
+            srcSet={`
+                ${src}/${type}/ps5_300w.${type} 300w,
+                ${src}/${type}/ps5_592w.${type} 592w,
+                ${src}/${type}/ps5_800w.${type} 800w,
+                ${src}/${type}/ps5_1600w.${type} 1600w,
+              `}
+            type={`image/${type}`}
+          />
+        ))}
+        <img src={`${src}/jpg/ps5_1600w.jpg`} width={`${width}`} height={`${height}`} alt={alt} decoding="async" loading="lazy" />
+      </picture>
+    );
+  }
+}
---
+++ packages/components-image/src/components/core-image/readme.md
@@ -0,0 +1,20 @@
+# my-component
+
+
+
+<!-- Auto Generated Below -->
+
+
+## Properties
+
+| Property | Attribute | Description                                                                                                                                          | Type     | Default     |
+| -------- | --------- | ---------------------------------------------------------------------------------------------------------------------------------------------------- | -------- | ----------- |
+| `alt`    | `alt`     | The image alt tag. [MDN](https://developer.mozilla.org/en-US/docs/Web/HTML/Element/img#attr-alt)                                                     | `string` | `undefined` |
+| `height` | `height`  | The intrinsic height of the image, in pixels. Must be an integer without a unit. Overwritten with `height: auto` by default, but should be included. | `number` | `undefined` |
+| `src`    | `src`     | The image source path without file extension. On browsers supporting `srcset`, `src` is treated as the fallback image with a 1x pixel density.       | `string` | `undefined` |
+| `width`  | `width`   | The intrinsic width of the image, in pixels. Must be an integer without a unit.                                                                      | `number` | `undefined` |
+
+
+----------------------------------------------
+
+*Built with [StencilJS](https://stenciljs.com/)*
